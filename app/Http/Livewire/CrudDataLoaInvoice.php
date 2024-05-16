201f48cdd88b67d3fb9d7030771bdcaffc762e6c
Khoirul Anam <92700194+KhoirulAnam78@users.noreply.github.com>
2024-05-16 09:17:55 +0700

    update mount kop

--- app/Http/Livewire/CrudDataLoaInvoice.php
+++ app/Http/Livewire/CrudDataLoaInvoice.php
@@ -160,23 +160,6 @@ class CrudDataLoaInvoice extends Component
 
     public function mount(){
         $this->kop = $this->getValue('kop');
-        if($this->kop){
-            $dom = new \domdocument();
-            $dom->loadHtml($this->kop, LIBXML_NOWARNING | LIBXML_NOERROR);
-    
-            //identify img element
-            $images = $dom->getelementsbytagname('img');
-
-            foreach ($images as $img) {
-                $data = $img->getattribute('src');
-                $path2 = "data:image/png;base64,".base64_encode(file_get_contents($data));
-                $img->removeattribute('src');
-                $img->setattribute('src', $path2);
-
-            }
-            $this->kop = $dom->saveHtml();
-        }
-
         $this->ttd_loa = $this->getValue('ttd_loa');
         $this->ttd_invoice = $this->getValue('ttd_invoice');
         $this->ttd_receipt = $this->getValue('ttd_receipt');
