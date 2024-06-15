f543c9b298fc483bd7e07b318214c66d50535670
Khoirul Anam <92700194+KhoirulAnam78@users.noreply.github.com>
2024-06-15 08:20:41 +0700

    setup laravel spatie

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -36,6 +36,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            'route.permission'
         ],
 
         'api' => [
@@ -64,5 +65,7 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        // custom by developer
+        'route.permission' => \App\Http\Middleware\RouteMiddleware::class,
     ];
 }
