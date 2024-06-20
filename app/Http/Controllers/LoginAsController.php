50ba70d1f90b2efb8ef244a2a807d8048e44a70a
Khoirul Anam <92700194+KhoirulAnam78@users.noreply.github.com>
2024-06-20 10:27:06 +0700

    log login as

--- app/Http/Controllers/LoginAsController.php
+++ app/Http/Controllers/LoginAsController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Utils\LogActivity;
 use Illuminate\Http\Request;
 
 class LoginAsController extends Controller
 {
     public function loginAs(Request $request){
         $user = User::where('email',$request->user_login_as)->first();
+        LogActivity::addLog("Login as ".$request->user_login_as);
         session()->flush();
         session()->put('main_user',$request->user_request_login_as);
         session()->put('login_web_59ba36addc2b2f9401580f014c7f58ea4e30989d',$user->id);
@@ -17,6 +19,7 @@ class LoginAsController extends Controller
 
     public function logoutAs(Request $request){
         $user = User::where('email',$request->main_user)->first();
+        LogActivity::addLog("Logout as ".auth()->user()->email);
         session()->flush();
         session()->put('login_web_59ba36addc2b2f9401580f014c7f58ea4e30989d',$user->id);
         return redirect()->to('dashboard');
